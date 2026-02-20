80b6efd43931af7a32707f5ba13a6d44b5dea32a
HubaNet <140731875+HubaNet@users.noreply.github.com>
2026-02-20 23:29:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let  picNum = 1;
-      let  textNum = 1;
-function changeSrc(){
+let picNum = 1;
+let textNum = 1;
 
-document.getElementById("picnump").innerHTML = `${textNum}/10`
-    document.getElementById("pictureGallery").src = `${picNum}.jpg`
-    ++picNum;
-++textNum;
-    
-    if (picNum > 10){picNum=1};
-    if (textNum > 10){textNum=1};
+function fwd() {
+    picNum++;
+    textNum++;
 
+    if (picNum > 10) picNum = 1;
+    if (textNum > 10) textNum = 1;
+
+    document.getElementById("picnump").innerHTML = `${textNum}/10`;
+    document.getElementById("pictureGallery").src = `${picNum}.jpg`;
+}
+
+function bck() {
+    picNum--;
+    textNum--;
+
+    if (picNum < 1) picNum = 10;
+    if (textNum < 1) textNum = 10;
+
+    document.getElementById("picnump").innerHTML = `${textNum}/10`;
+    document.getElementById("pictureGallery").src = `${picNum}.jpg`;
 }
